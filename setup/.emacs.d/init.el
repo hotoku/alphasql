60f5696243aba8576e7573157c2e994a1083714b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-23 23:53:26 +0000

    package update

---
+++ setup/.emacs.d/init.el
@@ -0,0 +1,57 @@
+;;; init.el -- initial setting up process -*- lexical-binding: t -*-
+
+
+;;; Commentary:
+
+
+;;; Code:
+
+
+;;; package
+(require 'package)
+(package-initialize)
+(let ((orig-value package-check-signature))
+  (setq package-check-signature nil)
+  (package-install 'gnu-elpa-keyring-update)
+  (setq package-check-signature orig-value))
+
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+
+
+;;; package-refresh-contents
+;; Package-refresh-contents if last update is older than or equal to yesterday.
+(let* ((path (expand-file-name ".date-of-last-package-refresh-contents" user-emacs-directory))
+       (today (format-time-string "%Y-%m-%d"))
+       (last-date (when (file-exists-p path)
+                    (with-temp-buffer
+                      (insert-file-contents path)
+                      (buffer-substring 1 11))))
+       (should-update (or (not last-date)
+                          (string< last-date today))))
+  (when should-update
+    (package-refresh-contents)
+    (with-temp-buffer
+      (insert today)
+      (write-region (point-min) (point-max) path))))
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (gnu-elpa-keyring-update))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;;; init.el ends here
