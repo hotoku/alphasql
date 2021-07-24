7ec223b1bc84b7129d7f799133d2022bd2c0eb0a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-24 02:04:06 +0000

    wip

--- setup/.emacs.d/init.el
+++ setup/.emacs.d/init.el
@@ -26,3 +26,15 @@ The last executing date is recorded in the FILENAME in `user-emacs-directory.'"
   (yh-doit-if-not-yet-today 'package-refresh-contents
                             ".date-of-last-package-refresh-contents")
   (package-install 'gnu-elpa-keyring-update))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (gnu-elpa-keyring-update))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
