977db123484e5ed23542eb131cbeb477f284e2a7
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-24 02:02:48 +0000

    wip

--- setup/.emacs.d/init.el
+++ setup/.emacs.d/init.el
@@ -1,67 +1,28 @@
-;;; init.el -- initial setting up process -*- lexical-binding: t -*-
+(defun yh-doit-if-not-yet-today (func filename)
+  "Call FUNC if it is not executed today.
+The last executing date is recorded in the FILENAME in `user-emacs-directory.'"
+  (let* ((path (expand-file-name filename user-emacs-directory))
+         (today (format-time-string "%Y-%m-%d"))
+         (last-date (when (file-exists-p path)
+                      (with-temp-buffer
+                        (insert-file-contents path)
+                        (buffer-substring 1 11))))
+         (should-update (or (not last-date)
+                            (string< last-date today))))
+    (when should-update
+      (funcall func)
+      (with-temp-buffer
+        (insert today)
+        (write-region (point-min) (point-max) path)))))
 
-
-;;; Commentary:
-
-
-;;; Code:
-
-;;; basics
-(show-paren-mode t)
-
-
-;;; package
 (require 'package)
-
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
 
-(let ((orig-value package-check-signature))
-  (setq package-check-signature nil)
-
-  (let* ((path (expand-file-name ".date-of-last-package-refresh-contents" user-emacs-directory))
-	 (today (format-time-string "%Y-%m-%d"))
-	 (last-date (when (file-exists-p path)
-		      (with-temp-buffer
-			(insert-file-contents path)
-			(buffer-substring 1 11))))
-	 (should-update (or (not last-date)
-			    (string< last-date today))))
-    (when should-update
-      (package-refresh-contents)
-      (with-temp-buffer
-	(insert today)
-	(write-region (point-min) (point-max) path))))
-
-  (package-install 'gnu-elpa-keyring-update)
-
-  (setq package-check-signature orig-value))
-
-
-;;; use-package initialize
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t) ; automatically install missing packages
-
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (gnu-elpa-keyring-update))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
-;;; init.el ends here
+(let ((package-check-signature nil))
+  (yh-doit-if-not-yet-today 'package-refresh-contents
+                            ".date-of-last-package-refresh-contents")
+  (package-install 'gnu-elpa-keyring-update))
