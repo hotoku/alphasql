a040d314d05b4673e03dba22ad2e1554bb8bb34e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-24 00:27:26 +0000

    this works

--- setup/.emacs.d/init.el
+++ setup/.emacs.d/init.el
@@ -6,36 +6,39 @@
 
 ;;; Code:
 
+;;; basics
+(show-paren-mode t)
+
 
 ;;; package
 (require 'package)
-(package-initialize)
-(let ((orig-value package-check-signature))
-  (setq package-check-signature nil)
-  (package-install 'gnu-elpa-keyring-update)
-  (setq package-check-signature orig-value))
 
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
+(package-initialize)
 
+(let ((orig-value package-check-signature))
+  (setq package-check-signature nil)
 
-;;; package-refresh-contents
-;; Package-refresh-contents if last update is older than or equal to yesterday.
-(let* ((path (expand-file-name ".date-of-last-package-refresh-contents" user-emacs-directory))
-       (today (format-time-string "%Y-%m-%d"))
-       (last-date (when (file-exists-p path)
-                    (with-temp-buffer
-                      (insert-file-contents path)
-                      (buffer-substring 1 11))))
-       (should-update (or (not last-date)
-                          (string< last-date today))))
-  (when should-update
-    (package-refresh-contents)
-    (with-temp-buffer
-      (insert today)
-      (write-region (point-min) (point-max) path))))
+  (let* ((path (expand-file-name ".date-of-last-package-refresh-contents" user-emacs-directory))
+	 (today (format-time-string "%Y-%m-%d"))
+	 (last-date (when (file-exists-p path)
+		      (with-temp-buffer
+			(insert-file-contents path)
+			(buffer-substring 1 11))))
+	 (should-update (or (not last-date)
+			    (string< last-date today))))
+    (when should-update
+      (package-refresh-contents)
+      (with-temp-buffer
+	(insert today)
+	(write-region (point-min) (point-max) path))))
+
+  (package-install 'gnu-elpa-keyring-update)
+
+  (setq package-check-signature orig-value))
 
 
 ;;; use-package initialize
